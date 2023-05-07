9a5932eea4f3702d2cb25988df7e4462ca55d131
Sanjita-Kumar <101909282+Sanjita-Kumar@users.noreply.github.com>
2023-05-07 07:26:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-# get data from the internet
-# yahoo finance
-
 import pandas_datareader as data
 import datetime as dt
-
-start = dt.datetime(2022, 4, 1)
-end = dt.datetime(2022, 5, 30)
-data = data.get_data_yahoo('AAPL', start, end)
-
-print(data)
 
 from datetime import datetime
 import yfinance as yf
 import pandas_datareader.data as pdr
 import pandas as pd
+
+
+start = dt.datetime(2022, 4, 1)
+end = dt.datetime(2022, 5, 30)
+data = data.get_data_yahoo('AAPL', start, end)
+print(data)
 
 yf.pdr_override()
 
@@ -24,7 +21,6 @@ aaplprices = pdr.get_data_yahoo("AAPL", start, end)
 
 del aaplprices['Volume']
 aaplprices.head()
-#aaplprices is a series object
 
 aaplprices.values.tolist()
 
